a2d3a773b76ad890572745e98361b2509b4ac97c
Danielly <daniellyfreitasc@hotmail.com>
2024-05-09 18:40:30 -0300

    feat: Adiciona rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListEpComponent } from './features/list-ep/list-ep.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: ListEpComponent,
+}];
