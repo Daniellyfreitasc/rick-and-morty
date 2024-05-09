d05fdef70c5d4ffd55897a26d503f4c27f6f6221
Danielly <daniellyfreitasc@hotmail.com>
2024-05-09 17:35:07 -0300

    feat: adciona mat-icon ao componente menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
